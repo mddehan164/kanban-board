83fb72b06dec6f82216cc0d05082ef2722d90aa8
MD Dehan <mddehan164@gmail.com>
2025-12-14 18:05:53 +0600

    dragable dove

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const boardChild = document.getElementById("board").children;
+let draggedElement = null;
 
-console.log(boardChild);
+const dragEvent = (element) => {
+  element.addEventListener("drop", (elem) => {
+    elem.preventDefault();
+    element.classList.remove("drag");
+    // console.log(draggedElement, element);
+    element.appendChild(draggedElement);
+  });
+
+  element.addEventListener("dragover", (elem) => {
+    elem.preventDefault();
+  });
+  element.addEventListener("dragenter", () => {
+    element.classList.add("drag");
+  });
+  element.addEventListener("dragleave", () => {
+    element.classList.remove("drag");
+  });
+};
+
+for (let i = 0; i < boardChild.length; i++) {
+  const element = boardChild[i];
+  dragEvent(element);
+}
+
+const columns = document.querySelectorAll(".board-child");
+
+columns.forEach((col) => {
+  col.addEventListener("drag", (e) => {
+    draggedElement = e.target;
+  });
+});
